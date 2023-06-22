f1a7d149fe5763ee7e400718522b3db3b3fea1a9
DESPIRro <93382641+DESPIRro@users.noreply.github.com>
2023-06-22 22:18:13 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://testuser:5uc2TGRacpQBmO8f@cluster0.rw2zlyy.mongodb.net/?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
